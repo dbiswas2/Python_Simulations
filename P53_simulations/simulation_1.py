aaf219945625f30867b3d42257f372bfd2c6331c
debojyoti <dbiswas2@jhu.edu>
2016-05-18 17:21:14 -0400

    lambda function is used

--- P53_simulations/simulation_1.py
+++ P53_simulations/simulation_1.py
@@ -34,12 +34,12 @@ def gen_model():
     vsysX = smodel.Volsys('vsysX', mdl)
     vsysY = smodel.Volsys('vsysY', mdl)
 
-    #
-    # lambda B_c: math.exp(B_c / (1 + B_c))
-    # lambda B_n: math.exp(B_n / (1 + B_n))
 
-    R_Ac_f = smodel.SReac('R_Ac_f', vsysX, lhs = [C_c], rhs = [A_c], k=Lambda(B_c))
-    R_An_f = smodel.SReac('R_An_f', vsysY, lhs = [C_n], rhs=  [0.1, A_n], k=Lambda(B_n))
+    f0 = lambda X: math.exp(X / (1 + X))
+
+
+    R_Ac_f = smodel.SReac('R_Ac_f', vsysX, lhs = [C_c, B_c], rhs = [A_c, B_c], k = lambda X:B_c)
+    R_An_f = smodel.SReac('R_An_f', vsysY, lhs = [C_n, B_n], rhs=  [0.1, A_n, B_n], k = lambda X:B_n)
 
     R_Bc_f = smodel.SReac('R_Bn_f', vsysX, lhs = [B_c], rhs = [0], k = 0.1)
     R_Bn_f = smodel.SReac('R_Bn_f', vsysX, lhs = [B_n], rhs = [0], k = 0.1)
